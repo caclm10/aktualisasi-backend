6af5f2bf8870806443130d8987e9f40fa80272c1
Lewin Xander <lewinxander@gmail.com>
2025-12-19 08:42:30 +0700

    feat: add macros for NanoID primary and foreign keys in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Schema\ForeignIdColumnDefinition;
+use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +22,31 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        JsonResource::withoutWrapping();
+
+        // 1. Macro untuk Primary Key NanoID
+        Blueprint::macro("nanoid", function (
+            $column = "id",
+            $length = 21,
+        ): \Illuminate\Database\Schema\ColumnDefinition {
+            // $this di sini mengacu pada instance Blueprint
+            return $this->char($column, $length)->primary();
+        });
+
+        // 2. Macro Foreign Key
+        Blueprint::macro("foreignNanoid", function (
+            $column,
+            $length = 21,
+        ): \Illuminate\Database\Schema\ForeignIdColumnDefinition {
+            // Kita return instance ForeignIdColumnDefinition
+            // supaya bisa pakai method ->constrained() nanti
+            return $this->addColumnDefinition(
+                new ForeignIdColumnDefinition($this, [
+                    "type" => "char", // <--- WAJIB CHAR (Karena NanoID itu String)
+                    "name" => $column,
+                    "length" => $length, // Panjang harus sama dengan PK tabel induk (21)
+                ]),
+            );
+        });
     }
 }
