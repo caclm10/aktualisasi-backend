2adce20121680854829f4ee3bb7d3f1f669830ad
Lewin Xander <lewinxander@gmail.com>
2025-12-22 16:49:15 +0700

    feat: add routes for Asset, Office, and Room resources with CRUD operations

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,34 @@
 <?php
 
+use App\Http\Controllers\AssetController;
+use App\Http\Controllers\OfficeController;
+use App\Http\Controllers\RoomController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(["auth:sanctum"])->group(function () {
     Route::get("/user", [UserController::class, "index"]);
+
+    Route::apiResource("assets", AssetController::class)->only([
+        "index",
+        "store",
+        "show",
+        "update",
+        "destroy",
+    ]);
+
+    Route::resource("offices", OfficeController::class)->only([
+        "index",
+        "store",
+        "show",
+        "update",
+        "destroy",
+    ]);
+
+    Route::resource("offices.rooms", RoomController::class)->only([
+        "index",
+        "store",
+        "update",
+        "destroy",
+    ]);
 });
